297f4a5042648f15c2daa9376999a778c2ec8e4d
sookjin_k <jinssook.kim@gmail.com>
2026-04-21 20:53:27 +0900

    Add files via upload

---
+++ lab_python/src/myutil1/addition.py
@@ -0,0 +1,9 @@
+def plus(x, y):
+    return x + y
+
+print(__name__)
+
+
+if __name__ == '__main__':
+    result = plus(1, 2)
+    print(result)
---
+++ lab_python/src/myutil1/subtraction.py
@@ -0,0 +1,9 @@
+def minus(x, y):
+    return x - y
+
+
+print(__name__)
+
+if __name__ == '__main__':
+    print(minus(3, 2))
+

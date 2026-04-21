78d12a00289109a8e5122d0295659449b03d051c
sookjin_k <jinssook.kim@gmail.com>
2026-04-21 21:37:09 +0900

    Add files via upload

---
+++ lab_python/main.py
@@ -0,0 +1,24 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'안녕하세요, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+x = 1
+if x % 2:
+    print('홀수')
+else:
+    print('짝수')
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('여러분')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
